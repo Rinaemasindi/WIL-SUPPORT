ac601fb41b22c032ad64170d2b5dfc47c8b765ee
Rinaemasindi <Rinaemasindi@outlook.com>
2022-11-05 20:48:42 +0200

    first commit

---
+++ wilsupport/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wilsupport project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wilsupport.settings')
+
+application = get_wsgi_application()
